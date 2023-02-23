cf702f26821ae3e2785cbd31f6d720658dee5860
Orxan228 <7ltdajp.code.edu.az>
2023-02-23 18:37:10 +0400

    Modified js file

--- task2.22.2023.js
+++ task2.22.2023.js
@@ -5,4 +5,4 @@ let myUndef = undefined;
 let myNull = null;
 let mySymbol = {anySymbol:"Symbol"};
 let myObject = {x:5,smth:3.4,someWord:"Smth"};
-window.print(myString+myNumber+myBool+myUndef+myNull+mySymbol+myObject);
+console.log(myString + myNumber + myBool + myUndef + myNull + mySymbol + myObject)
