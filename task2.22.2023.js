fd484b340cf398ee01d37452228d57828e28a78e
Orxan002 <7ltdajp@code.edu.az>
2023-02-23 18:28:22 +0400

    Added C# and js files

---
+++ task2.22.2023.js
@@ -0,0 +1,8 @@
+let myString = "What a nice day";
+let myNumber = 256;
+let myBool = true;
+let myUndef = undefined;
+let myNull = null;
+let mySymbol = {anySymbol:"Symbol"};
+let myObject = {x:5,smth:3.4,someWord:"Smth"};
+window.print(myString+myNumber+myBool+myUndef+myNull+mySymbol+myObject);
---
+++ test.js
@@ -0,0 +1,9 @@
+var surname = "RandomSurname"
+var age = 20;
+var IsMarried = true;
+var symbol = 'a';
+var price = 9.5;
+price=2;
+
+
+console.log(price);
